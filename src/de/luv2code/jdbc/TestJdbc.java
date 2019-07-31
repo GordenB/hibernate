8c82b73638befeb49b8aa031f0fe6c3d7543b096
Gorden Biemüller <Gorden.biem@Googlemail.com>
2019-07-31 20:35:11 +0200

    Annotate Java Class

--- src/de/luv2code/jdbc/TestJdbc.java
+++ src/de/luv2code/jdbc/TestJdbc.java
@@ -7,7 +7,7 @@ public class TestJdbc {
 
 	public static void main(String[] args) {
 
-		String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=Europe/Berlin";
+		String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false";
 		String user = "hbstudent";
 		String pass = "hbstudent";
 		
