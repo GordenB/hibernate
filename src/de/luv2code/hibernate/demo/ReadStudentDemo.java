4c1f8cf53adb432ed98e64b077e7e7cbc027725e
Gorden Biemüller <Gorden.biem@Googlemail.com>
2019-08-04 13:18:31 +0200

    add UpdateStudentDemo with Hibernate

--- src/de/luv2code/hibernate/demo/ReadStudentDemo.java
+++ src/de/luv2code/hibernate/demo/ReadStudentDemo.java
@@ -18,36 +18,33 @@ public class ReadStudentDemo {
 		// create session
 		Session session = factory.getCurrentSession();
 		try {
-			// create a student
-			System.out.println("Creating new student object...");
-			Student tempStudent = new Student("Daffy", "Duck","daffy@luv2code.de");
-			
-			// start a transaction
-			session.beginTransaction();
-			
-			// save the student object
-			System.out.println("Saving the student...");
-			System.out.println(tempStudent);
-			session.save(tempStudent);
-			
-			// commit transaction
-			session.getTransaction().commit();
-			
-			// MY NEW CODE
-			
-			// find out the student´s id: primary key
-			System.out.println("Saved student. Generated id: " + tempStudent.getId());
+			int studentId = 1;
 			
 			// now get a new session  and start transaction
 			session = factory.getCurrentSession();
 			session.beginTransaction();
 			
 			// retrieve student based on the id: primary
-			System.out.println("\nGetting student with id: " + tempStudent.getId());
+			System.out.println("\nGetting student with id: " + studentId);
 			
-			Student myStudent = session.get(Student.class, tempStudent.getId());
+			Student myStudent = session.get(Student.class, studentId);
 			
-			System.out.println("Get complete: " + myStudent);
+			System.out.println("Updating student...");
+			myStudent.setFirstName("Scooby");
+
+			//commit the transaction
+			session.getTransaction().commit();
+			
+			// New Code
+			
+			session = factory.getCurrentSession();
+			session.beginTransaction();
+			
+			// update email for all students
+			System.out.println("Updating email from all students");
+			
+			session.createQuery("update Student set email='foo@gmail.com'")
+								.executeUpdate();
 			
 			//commit the transaction
 			session.getTransaction().commit();
